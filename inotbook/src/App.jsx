1e461730a7e38e3e8a970829c347ab076a9d4fc7
sumitdwivedi01 <sumitdwivedi056@gmail.com>
2025-08-20 15:18:56 +0530

    Learned a lot about context api

--- inotbook/src/App.jsx
+++ inotbook/src/App.jsx
@@ -1,11 +1,26 @@
 
 import './App.css'
-
+import { Routes, Route ,Link} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/home';
+import About from './components/about';
+import NoteState from './components/context/notes/NoteState';
 function App() {
 
   return (
+    <NoteState>
     <>
+
+    <Navbar/>
+    <h1>this is iNotebook</h1>
+    <Routes>
+  
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+
+      </Routes>      
     </>
+    </NoteState>
   )
 }
 
